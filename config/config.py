992f7d35be22b17f1822424f93df8cd1dc7eee65
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-05-16 17:02:26 -0300

    init: initial commit

--- config/config.py
+++ config/config.py
@@ -38,7 +38,7 @@ SONG_DOWNLOAD_DURATION = int(
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001285436101"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001308762937"))
 
 # A name for your Music bot.
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Winx Bot")
