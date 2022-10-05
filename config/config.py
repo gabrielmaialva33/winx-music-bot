44eb592142972176cdc3b493eab81f309484844f
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-10-05 15:58:31 -0300

    init: initial commit

--- config/config.py
+++ config/config.py
@@ -66,11 +66,11 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", None
-)  # Example:- https://t.me/TheWinx
+    "SUPPORT_CHANNEL", "https://t.me/clubdaswinxcanal"
+)  # Example:- https://t.me/politicament
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", None
-)  # Example:- https://t.me/WinxSupport
+    "SUPPORT_GROUP", "https://t.me/polclubdaswinx"
+)  # Example:- https://t.me/politicament
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
