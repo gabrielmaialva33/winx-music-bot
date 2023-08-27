39ac5e364f83927db3bcdbf1cf37a0f873b4b647
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-08-27 11:31:21 -0300

    refactor: main global

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 by mrootx@Github, < https://github.com/gabrielmaialva33 >.
+# Copyright (C) 2021-2023 by Maia, < https://github.com/gabrielmaialva33 >.
 #
 # This file is part of < https://github.com/gabrielmaialva33/winx-music-bot > project,
 # and is released under the "GNU v3.0 License Agreement".
