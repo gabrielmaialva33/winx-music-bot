c4e0145406b71c699eaa36fbd29234f0f7355667
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-09-03 16:20:24 -0300

    feat: implement wifu

---
+++ WinxMusic/plugins/wifu/nekos.py
@@ -0,0 +1,7 @@
+import logging
+from pyrogram import filters
+import requests
+from WinxMusic import app
+from strings import get_command
+
+NEKO_COMMAND = get_command("NEKO_COMMAND")
--- WinxMusic/plugins/wifu/wifu.py
+++ WinxMusic/plugins/wifu/wifu.py
@@ -6,23 +6,12 @@ from strings import get_command
 
 WIFU_COMMAND = get_command("WIFU_COMMAND")
 
-# Define your bot token
-TOKEN = "409K8eK4Ae_d6URhndD4RPyLkZ0pFeOp387ZATl8exox71TdYJpCkHj3AiQljp50X6gkptKrKQUCp419unNxddZlgEeGzwPxbTwYP21OcmbLjNmIIFVteTALHIbaVJGBWwEp9FoQUBAxNq5KPMKMvMv0Hr1ctzJMHEAiZMWZvhE"
-
 # Define the API endpoint to fetch waifu images
 API_ENDPOINT_1 = "https://api.waifu.pics/sfw/waifu"
-API_ENDPOINT_2 = "https://nekos.best/api/v2/waifu/"
-
-
-headers = {
-    'Accept-Version': 'v5',
-    'Authorization': 'Bearer ' + TOKEN,
-}
-
 
 def waifu():
     try:
-        response = requests.get(API_ENDPOINT_1, headers=headers)
+        response = requests.get(API_ENDPOINT_1)
         data = response.json()
         image_url = data["url"]
         return image_url
