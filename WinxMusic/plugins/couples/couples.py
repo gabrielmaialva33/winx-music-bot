b3c1c41d2254d2119ad7167d0a2c1cdbb3d5ef1a
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-09-07 21:28:08 -0300

    chore: linting code

--- WinxMusic/plugins/couples/couples.py
+++ WinxMusic/plugins/couples/couples.py
@@ -38,40 +38,40 @@ COUPLE_COMMAND = get_command("COUPLE_COMMAND")
 
 @app.on_message(filters.command(COUPLE_COMMAND) & filters.group)
 async def couple(_, message):
-        chat_id = message.chat.id
-        is_selected = await get_couple(chat_id, date=today)
+    chat_id = message.chat.id
+    is_selected = await get_couple(chat_id, date=today)
 
-        if not is_selected:
-            list_of_users = []
-            async for i in app.get_chat_members(message.chat.id, limit=100):
-                if not i.user.is_bot:
-                    list_of_users.append(i.user.id)
-            if len(list_of_users) < 2:
-                return await message.reply_text("Não há membros suficientes para escolher um casal")
+    if not is_selected:
+        list_of_users = []
+        async for i in app.get_chat_members(message.chat.id, limit=100):
+            if not i.user.is_bot:
+                list_of_users.append(i.user.id)
+        if len(list_of_users) < 2:
+            return await message.reply_text("Não há membros suficientes para escolher um casal")
+        c1_id = random.choice(list_of_users)
+        c2_id = random.choice(list_of_users)
+        while c1_id == c2_id:
             c1_id = random.choice(list_of_users)
-            c2_id = random.choice(list_of_users)
-            while c1_id == c2_id:
-                c1_id = random.choice(list_of_users)
-            c1_mention = (await app.get_users(c1_id)).mention
-            c2_mention = (await app.get_users(c2_id)).mention
+        c1_mention = (await app.get_users(c1_id)).mention
+        c2_mention = (await app.get_users(c2_id)).mention
 
-            couple_selection_message = f"""**⚜ 🌈 Casal do dia 🎡**
+        couple_selection_message = f"""**⚜ 🌈 Casal do dia 🎡**
 
 {c1_mention} + {c2_mention} = ❤️‍🔥
 Novos casais serão escolhidos amanhã às 12h {tomorrow}"""
-            await app.send_photo(message.chat.id, photo="https://telegra.ph/file/908be770f3a34834379f1.png",
-                                 caption=couple_selection_message)
-            couple = {"c1_id": c1_id, "c2_id": c2_id}
-            await save_couple(chat_id, today, couple)
+        await app.send_photo(message.chat.id, photo="https://telegra.ph/file/908be770f3a34834379f1.png",
+                             caption=couple_selection_message)
+        couple = {"c1_id": c1_id, "c2_id": c2_id}
+        await save_couple(chat_id, today, couple)
 
-        elif is_selected:
-            c1_id = int(is_selected["c1_id"])
-            c2_id = int(is_selected["c2_id"])
-            c1_name = (await app.get_users(c1_id)).mention
-            c2_name = (await app.get_users(c2_id)).mention
-            couple_selection_message = f"""⚜ **Casal do dia :**
+    elif is_selected:
+        c1_id = int(is_selected["c1_id"])
+        c2_id = int(is_selected["c2_id"])
+        c1_name = (await app.get_users(c1_id)).mention
+        c2_name = (await app.get_users(c2_id)).mention
+        couple_selection_message = f"""⚜ **Casal do dia :**
 
 {c1_name} + {c2_name} = ❤️‍🔥
 Novo casais serão escolhidos amanhã às 12h {tomorrow}"""
-            await app.send_photo(message.chat.id, photo="https://telegra.ph/file/908be770f3a34834379f1.png",
-                                 caption=couple_selection_message)
+        await app.send_photo(message.chat.id, photo="https://telegra.ph/file/908be770f3a34834379f1.png",
+                             caption=couple_selection_message)
