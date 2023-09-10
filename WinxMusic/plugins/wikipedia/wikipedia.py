6aba5207d550597ee1ae832974ae5e37678e84d2
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-09-10 02:48:40 -0300

    chore: linting code

--- WinxMusic/plugins/wikipedia/wikipedia.py
+++ WinxMusic/plugins/wikipedia/wikipedia.py
@@ -1,8 +1,5 @@
-from io import BytesIO
-
 import requests
-from PIL import Image, ImageDraw
-from pyrogram import Client, filters
+from pyrogram import filters
 
 from WinxMusic import app
 from config import BANNED_USERS
@@ -18,7 +15,7 @@ WIKIPEDIA_COMMAND = get_command("WIKIPEDIA_COMMAND")
 
 
 @app.on_message(filters.command(WIKIPEDIA_COMMAND) & filters.group & ~BANNED_USERS)
-def handle_search(client: Client, message):
+def handle_search(_, message):
     query = message.text.split(None, 1)[1]
     url = f"https://pt.wikipedia.org/api/rest_v1/page/summary/{query}"
     response = requests.get(url)
@@ -29,13 +26,4 @@ def handle_search(client: Client, message):
     else:
         result = "Desculpe, não encontrei nada."
 
-    image = Image.new('RGB', (500, 500))
-    draw = ImageDraw.Draw(image)
-    draw.text((10, 10), result, fill=(255, 255, 255))
-
-    image_file = BytesIO()
-    image.save(image_file, 'PNG')
-    image_file.seek(0)
-
-    client.send_photo(message.chat.id, photo=image_file,
-                      caption=f"**Wikipedia: {query}** \n\n**Resultado:**\n{result}")
+    message.reply_text(result)
