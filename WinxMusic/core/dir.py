ff6d198ec5d76130f1d748f2b1e338dfac9a6b23
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-09-25 01:21:35 -0300

    refactor: core dir

--- WinxMusic/core/dir.py
+++ WinxMusic/core/dir.py
@@ -5,6 +5,18 @@ from os import listdir, mkdir
 
 from config import TEMP_DB_FOLDER
 
+# remove all files on startup  that contains these extentions
+files = [
+    ".jpg",
+    ".jpeg",
+    ".mp3",
+    ".m4a",
+    ".mp4",
+    ".webm",
+    ".png",
+    ".session",
+    ".session-journal",
+]
 
 def dirr():
     assets_folder = "assets"
@@ -18,14 +30,7 @@ def dirr():
         sys.exit()
 
     for file in os.listdir():
-        if (
-                file.endswith(".jpg")
-                or file.endswith(".jpeg")
-                or file.endswith(".mp3")
-                or file.endswith(".png")
-                or file.endswith(".session")
-                or file.endswith(".session-journal")
-        ):
+        if any(file.endswith(ext) for ext in files):
             os.remove(file)
 
     if downloads_folder not in listdir():
